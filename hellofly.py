87995acf95ba5a923b86d4c9ef47c7adfee16d64
Jacob Mccalip <jacobmccalip@gmail.com>
2022-11-27 18:47:16 -0600

    Added Nav, "blog" template, and "workout" template

--- hellofly.py
+++ hellofly.py
@@ -1,9 +1,36 @@
+import os
+#import flask, render_template
 from flask import Flask, render_template
+import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, \
+    current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import find_dotenv, load_dotenv
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+#db = SQLAlchemy(app)
 
+#login_manager = LoginManager()
+#login_manager.init_app(app)
+
+@app.route("/workouts")
+def workout():
+    # """this function renders the index page of the site
+    # TODO: finish project"""
+    return render_template('workouts.html')
+
+@app.route("/")
+def index():
+    # """this function renders the index page of the site
+    # TODO: finish project"""
+    return render_template('dashboard.html')
+
+
+
+#with app.app_context():
+#    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True) 
