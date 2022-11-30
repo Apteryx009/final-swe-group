354ce391e5499238cecb36c6cc86434204c419e5
rduckworthiv <randall.duckworthiv@gmail.com>
2022-11-29 19:47:56 -0600

    Added basic login and signup functionality

--- hellofly.py
+++ hellofly.py
@@ -1,4 +1,5 @@
 import os
+import flask
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, \
@@ -18,16 +19,15 @@ db = SQLAlchemy(app)
 class endUser(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), unique=True, nullable=False)
-    passwordHash = db.Column(db.String(2), nullable=False)
+    passwordHash = db.Column(db.String(500), nullable=False)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password_hash):
         self.username = username
-        self.passwordHash = password
+        self.passwordHash = password_hash
 
 def createUser(new_user, password):
     """this function creates a new user"""
-    print("called create new user function")
-    user = endUser(new_user, password)
+    user = endUser(new_user, generate_password_hash(password))
     db.session.add(user)
     db.session.commit()
 
@@ -56,10 +56,47 @@ def Cal():
     # """
     return render_template('Cal.html')
 
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    if flask.request.method == 'GET':
+        # if method == GET, render login page
+        return render_template('login.html')
+    else:
+        # else, process login
+        # retrieve username and password from form fields
+        username = flask.request.form.get("username")
+        password = flask.request.form.get("password")
+
+        user = endUser.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.passwordHash, password):
+                login_user(user)
+                flask.flash("Login successful.")
+                return flask.redirect(flask.url_for('index'))
+            else:
+                flask.flash("Incorrect password")
+        else:
+            flask.flash("User ID does not exist in database.")
+    username=""
+    password=""
+
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def create_an_account():
+    if flask.request.method == 'GET':
+        return render_template('signup.html')
+    else:
+        username = flask.request.form.get("username")
+        password = flask.request.form.get("password")
+
+        createUser(username, password)
+
+
+
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
-    print("test")
-    createUser("johnny test", "ps")
+
